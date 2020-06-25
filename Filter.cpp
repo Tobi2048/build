c0820dbd4f723a0c435c5388d1cf8b53014cd982
Tobi2048 <64415000+Tobi2048@users.noreply.github.com>
2020-06-25 09:52:51 +0200

    ober

--- Filter.cpp
+++ Filter.cpp
@@ -30,3 +30,82 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr flaechen_filter(pcl::PointCloud<pcl::PointXY
     pass.filter(*cl_fl_out);
     return(cl_fl_out);
 }
+
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr oberflaechen_filter(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int num)
+{
+
+
+    pcl::search::Search<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+    pcl::PointCloud <pcl::Normal>::Ptr normals(new pcl::PointCloud <pcl::Normal>);
+    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
+    normal_estimator.setSearchMethod(tree);
+    normal_estimator.setInputCloud(cloud);
+    normal_estimator.setKSearch(50);
+    normal_estimator.compute(*normals);
+
+    pcl::IndicesPtr indices(new std::vector <int>);
+    pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setInputCloud(cloud);
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(0.0, 1.0);
+    pass.filter(*indices);
+
+    pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> reg;
+    reg.setMinClusterSize(3000);
+    reg.setMaxClusterSize(1000000);
+    reg.setSearchMethod(tree);
+    reg.setNumberOfNeighbours(50);
+    reg.setInputCloud(cloud);
+    //reg.setIndices (indices);
+    reg.setInputNormals(normals);
+    reg.setSmoothnessThreshold(4.0 / 180.0 * M_PI);
+    reg.setCurvatureThreshold(1.0);
+
+    std::vector <pcl::PointIndices> clusters;
+    reg.extract(clusters);
+
+    std::cout << "Number of clusters is equal to " << clusters.size() << std::endl;
+    for (int i = 0; i < clusters.size(); i++)
+    {
+
+        std::cout << i << " cluster has " << clusters[i].indices.size() << " points." << std::endl;
+        std::cout << "These are the indices of the points of the initial" <<
+            std::endl << "cloud that belong to the first cluster:" << std::endl;
+
+
+        std::cout << std::endl;
+    }
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_stein(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PCDWriter writer;
+    int j = 0;
+    for (std::vector<pcl::PointIndices>::const_iterator it = clusters.begin(); it != clusters.end(); ++it)
+    {
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
+        for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit)
+            cloud_cluster->points.push_back(cloud->points[*pit]); //*
+        cloud_cluster->width = cloud_cluster->points.size();
+        cloud_cluster->height = 1;
+        cloud_cluster->is_dense = true;
+
+        std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size() << " data points." << std::endl;
+        std::stringstream ss;
+        ss << "cloud_cluster_" << j << ".pcd";
+        writer.write<pcl::PointXYZ>(ss.str(), *cloud_cluster, false); //*
+        j++;
+        if (num == j)
+            cloud_stein = cloud_cluster;
+    }
+
+
+    pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud();
+    pcl::visualization::CloudViewer viewer("Cluster viewer");
+    viewer.showCloud(colored_cloud);
+
+    while (!viewer.wasStopped())
+    {
+    }
+
+    return (cloud_stein);
+}
--- Filter.h
+++ Filter.h
@@ -6,5 +6,13 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+#include <vector>
+#include <pcl/search/search.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/segmentation/region_growing.h>
 
+pcl::PointCloud<pcl::PointXYZ>::Ptr oberflaechen_filter(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int num);
+pcl::PointCloud<pcl::PointXYZ>::Ptr flaechen_filter(pcl::PointCloud<pcl::PointXYZ>::Ptr cl_fl_in, float grenzw, std::string achse, float aufloesung);
 pcl::PointCloud<pcl::PointXYZ>::Ptr vox(pcl::PointCloud<pcl::PointXYZ>::Ptr cl_vox_in, float fakt);
